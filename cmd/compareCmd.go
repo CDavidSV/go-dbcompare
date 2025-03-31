b5a9c592f6de0eada2d9ba1757196352606ae8f5
CDavidSV <cdavidsv6@gmail.com>
2025-03-31 16:28:13 -0600

    added new flags for the compare command to avoid using a config file to connect to the databases

--- cmd/compareCmd.go
+++ cmd/compareCmd.go
@@ -21,20 +21,29 @@ var compareCmd = &cobra.Command{
 		configFilePath, _ := cmd.Flags().GetString("config")
 		outputPath, _ := cmd.Flags().GetString("output")
 		name, _ := cmd.Flags().GetString("name")
+		dsn1, _ := cmd.Flags().GetString("dsn1")
+		dsn2, _ := cmd.Flags().GetString("dsn2")
 
-		conf, err := helpers.LoadConfigurationFile(configFilePath)
-		if err != nil {
-			fmt.Println(config.ErrorStyle.Render("Error reading configuration file:"), err)
-			os.Exit(1)
+		db1Name := "DB1"
+		db2Name := "DB2"
+		if dsn1 == "" || dsn2 == "" {
+			conf, err := helpers.LoadConfigurationFile(configFilePath)
+			if err != nil {
+				fmt.Println(config.ErrorStyle.Render("Error reading configuration file:"), err)
+				os.Exit(1)
+			}
+
+			db1Name = conf.DB1.Name
+			db2Name = conf.DB2.Name
+
+			dsn1 = helpers.GetDataSourceName("postgres", conf.DB1.Username, conf.DB1.Password, conf.DB1.HostName, conf.DB1.Database, conf.DB1.Port, conf.DB1.Params)
+			dsn2 = helpers.GetDataSourceName("postgres", conf.DB2.Username, conf.DB2.Password, conf.DB2.HostName, conf.DB2.Database, conf.DB2.Port, conf.DB2.Params)
 		}
-
-		dsn1 := helpers.GetDataSourceName("postgres", conf.DB1.Username, conf.DB1.Password, conf.DB1.HostName, conf.DB1.Database, conf.DB1.Port, conf.DB1.Params)
-		dsn2 := helpers.GetDataSourceName("postgres", conf.DB2.Username, conf.DB2.Password, conf.DB2.HostName, conf.DB2.Database, conf.DB2.Port, conf.DB2.Params)
 
 		helpers.SaveCursorPosition()
 
 		s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
-		s.Suffix = fmt.Sprintf(config.InfoStyle.Render(" Connecting to %s"), conf.DB1.Name)
+		s.Suffix = fmt.Sprintf(config.InfoStyle.Render(" Connecting to %s"), db1Name)
 		s.Start()
 
 		// Connect to databases
@@ -42,29 +51,29 @@ var compareCmd = &cobra.Command{
 		s.Stop()
 		if err != nil {
 			helpers.ClearLine()
-			fmt.Printf(config.ErrorStyle.Render("Error connecting to %s\nError: %s\n"), conf.DB1.Name, err)
+			fmt.Printf(config.ErrorStyle.Render("Error connecting to %s\nError: %s\n"), db1Name, err)
 			os.Exit(1)
 		}
 		defer DB1.Close()
 
 		helpers.ClearLine()
-		fmt.Printf(config.SuccessStyle.Render("Connected to %s\n"), conf.DB1.Name)
+		fmt.Printf(config.SuccessStyle.Render("Connected to %s\n"), db1Name)
 		helpers.SaveCursorPosition()
 
-		s.Suffix = fmt.Sprintf(config.InfoStyle.Render(" Connecting to %s"), conf.DB2.Name)
+		s.Suffix = fmt.Sprintf(config.InfoStyle.Render(" Connecting to %s"), db2Name)
 		s.Start()
 
 		DB2, err := helpers.ConnectDB("postgres", dsn2)
 		s.Stop()
 		if err != nil {
 			helpers.ClearLine()
-			fmt.Printf(config.ErrorStyle.Render("Error connecting to %s\nError: %s\n"), conf.DB2.Name, err)
+			fmt.Printf(config.ErrorStyle.Render("Error connecting to %s\nError: %s\n"), db2Name, err)
 			os.Exit(1)
 		}
 		defer DB2.Close()
 
 		helpers.ClearLine()
-		fmt.Printf(config.SuccessStyle.Render("Connected to %s\n\n"), conf.DB2.Name)
+		fmt.Printf(config.SuccessStyle.Render("Connected to %s\n\n"), db2Name)
 		helpers.SaveCursorPosition()
 
 		s.Suffix = config.InfoStyle.Render(" Running comparison")
@@ -89,7 +98,7 @@ var compareCmd = &cobra.Command{
 			outputPath += name + ".xlsx"
 		}
 
-		err = helpers.SaveAsExcel(result, conf.DB1.Name, conf.DB2.Name, outputPath)
+		err = helpers.SaveAsExcel(result, db1Name, db2Name, outputPath)
 		if err != nil {
 			fmt.Println(config.ErrorStyle.Render("Error saving result file:"), err)
 			os.Exit(1)
@@ -103,4 +112,6 @@ func init() {
 	compareCmd.Flags().StringP("config", "c", "./db-compare-config.json", "path for the configuration file")
 	compareCmd.Flags().StringP("output", "o", "./", "path where the comparison result file is saved")
 	compareCmd.Flags().StringP("name", "n", "", "name of the comparison result file")
+	compareCmd.Flags().String("dsn1", "", "connection string for the first database")
+	compareCmd.Flags().String("dsn2", "", "connection string for the first database")
 }
